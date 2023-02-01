db44f65d40be54fd102b42c158472d66f4dfb026
Jacob Ortiz <jortiz@hellosign.com>
2023-02-01 17:08:05 -0500

    add tests

--- car_test.go
+++ car_test.go
@@ -1,1 +1,16 @@
 package foobar_test
+
+import "testing"
+
+func TestCar_GetMaker(t *testing.T) {
+	testcase := []string{"BMW", "Audi", "Tesla", "Ferrari"}
+
+	for _, tc := range testcase {
+		t.Run(tc, func(t *testing.T) {
+			if tc != tc {
+				t.Fail()
+			}
+		})
+	}
+
+}
