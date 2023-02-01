1288b6a17f3df42774a0d0e862f49d10b0034d91
Jacob Ortiz <jortiz@hellosign.com>
2023-02-01 16:32:03 -0500

    create car type with no tests

---
+++ car.go
@@ -0,0 +1,24 @@
+package foobar
+
+type Car struct {
+	Miles int
+	Year  int
+	Model string
+	Maker string
+}
+
+func (c *Car) GetYear() int {
+	return c.Year
+}
+
+func (c *Car) GetMiles() int {
+	return c.Miles
+}
+
+func (c *Car) GetModel() string {
+	return c.Model
+}
+
+func (c *Car) GetMaker() string {
+	return c.Maker
+}
